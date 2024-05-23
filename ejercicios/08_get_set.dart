d55154c5ab4356d3f885ee2302c3572b5abc19a3
Oscar Rojas <rojassanchezo63@gmail.com>
2024-05-22 18:50:34 -0600

    Ejercicios del 5 al 8

---
+++ ejercicios/08_get_set.dart
@@ -0,0 +1,25 @@
+void main(){
+  
+  final mySquare = Square(side: 10);
+  
+  mySquare._side = 2;
+  print('Area: ${mySquare.area}');
+  
+}
+
+class Square{
+  double _side; // side * side
+  
+  Square({required double side})
+    : assert(side >= 0, 'side must be >= 0'),
+  _side = side;
+  
+  double get area{
+    return _side * _side;
+  }
+  
+  set side(double value){
+    if (value < 0) throw 'value must be >= 0';
+    _side = value;
+  }
+}
